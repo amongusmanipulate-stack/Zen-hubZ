b1248a2e649cf364bcb5066d802fe86ee475602c
amongusmanipulate-stack <amongusmanipulate@gmail.com>
2026-09-15 06:46:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google AI Studio API Key (AQ. format supported)
-const API_KEY = "AQ.Ab8RN6JO1RglXWdFLKS0DNNv-Ry5_hkn019cZDMEiWnLCpRTVg"; 
+const API_KEY = AQ.Ab8RN6KpLSr_ctmOPDIun0sUbrBaqFs0kK23Je8pkqrTFYCXeQ 
 
 // Passcode Protection Logic (KINGVON - 3 Attempts)
 let attemptsLeft = 3;
